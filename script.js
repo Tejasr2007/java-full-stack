a11fc50f5dabb329611596d22ec9e48e8b7d388d
Tejasr2007 <ptejasree2007@gmail.com>
2026-06-11 12:13:50 +0530

    java script

---
+++ script.js
@@ -0,0 +1,40 @@
+let name="tejasree";
+var age=19;
+const height=5.4;
+console.log(age);
+console.log(age+5);
+console.log(age-3);
+console.log(age*2);
+console.log(age/2);
+console.log(age%2);
+age+=3
+console.log(age);
+age-=2;
+console.log(age);
+age*=2;
+console.log(age);
+age/=2;
+console.log(age);
+age%=2;
+console.log(age);
+let age1=30;
+console.log(age>age1);
+console.log(age<age1);
+console.log(age>=age1);
+console.log(age<=age1);
+console.log(age===age1);
+let food="pizza";
+let food1="dosa";
+let food2="idly";
+let food3="pori";
+if(food===food1){
+    console.log("both are nice foods");
+}
+else{
+    console.log("both are different foods");
+}
+for(let i=1;i<=30;)
+{
+    console.log("day "+i+" completed");
+    i++;
+}
