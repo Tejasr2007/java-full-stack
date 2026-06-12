947ed71fb205aa1844a9a5ffb189e421e9d26d65
Tejasr2007 <ptejasree2007@gmail.com>
2026-06-12 11:58:49 +0530

    happy

--- manipulation.js
+++ manipulation.js
@@ -1,2 +1,12 @@
-let great = document.getElementById("birthday-greeting");
-console.log(great.innerText);
+/* let great = document.getElementById("birthday-greeting");
+console.log(great.innerText); */
+let pen = document.getElementById("birthday-greeting");
+console.log(pen);
+let mouse= document.getElementById("birthday-greeting");
+console.log(mouse);
+let cable = document.getElementById("div");
+console.log(cable);
+let water = document.getElementById("#content");
+console.log(water);
+let queries = document.querySelector("p");
+console.log(queries);
